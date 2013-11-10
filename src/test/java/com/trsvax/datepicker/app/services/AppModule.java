6754d6db4f83186e8fbd325c9f056ed0dd262cb5
Barry Books <bfb@trsvax.com>
2013-11-10 08:46:41 -0600

    add symbols

--- src/test/java/com/trsvax/datepicker/app/services/AppModule.java
+++ src/test/java/com/trsvax/datepicker/app/services/AppModule.java
@@ -1,5 +1,16 @@
 package com.trsvax.datepicker.app.services;
 
+import org.apache.tapestry5.SymbolConstants;
+import org.apache.tapestry5.ioc.MappedConfiguration;
+
 public class AppModule {
+	
+    public static void contributeFactoryDefaults(MappedConfiguration<String, Object> configuration) {
+        configuration.override(SymbolConstants.HMAC_PASSPHRASE,"datepicker");
+    }
+    
+    public static void contributeApplicationDefaults(MappedConfiguration<String, Object> configuration) {
+        configuration.add(SymbolConstants.PRODUCTION_MODE, false);
+    }
 
 }
